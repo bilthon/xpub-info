b1e6e2ebad29f5dd830f4d4f3c1e96db51c4c67c
Nelson R. Perez <bilthon@gmail.com>
2020-04-18 12:54:28 -0500

    Basic structure

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,14 +1,54 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		justifyContent: 'center',
+		margin: '0.5em',
+		height: '95vh'
+	},
+	xpub: {
+		width: '80%',
+		height: '150pt',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center'
+	},
+	addresses: {
+		width: '80%'
+	}
+});
 
 class Main extends React.Component {
+	state = {
+		xpub: {
+			version: null,
+			depth: null,
+			parentFingerprint: null,
+			index: null,
+			chainCode: null,
+			key: null
+		}
+	}
+
 	render() {
+		const { classes } = this.props;
 		return (
-			<div>
-				<TextField variant='outlined' label='Extended Key'/>
+			<div className={classes.root}>
+				<div className={classes.xpub}>
+					<FormControl fullWidth>
+						<TextField variant='outlined' label='Extended Key'/>
+					</FormControl>
+				</div>
+				<div>
+
+				</div>
 			</div>
 		)
 	}
 }
 
-export default Main;
+export default withStyles(styles)(Main);
