f403adf55fd1653d7b18a7b6e2932ad066172fab
Nelson R. Perez <bilthon@gmail.com>
2020-04-18 12:29:50 -0500

    Introducing the Main container

---
+++ src/containers/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div>
+				<TextField variant='outlined' label='Extended Key'/>
+			</div>
+		)
+	}
+}
+
+export default Main;
