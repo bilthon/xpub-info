2deb2fb1b8430e9cbfe618f4e1487947a28bd78d
Nelson R. Perez <bilthon@gmail.com>
2020-08-09 14:33:27 -0500

    fix: removes unused imports

--- src/containers/AddressList/AddressList.js
+++ src/containers/AddressList/AddressList.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import PropTypes from 'prop-types';
 
 import { payments } from 'bitcoinjs-lib';
